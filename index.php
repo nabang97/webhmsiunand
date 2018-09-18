6865ab1b02230c3e18b852068b8fefc188e28102
nabang97 <parawitayolanda@gmail.com>
2018-09-18 17:11:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="fullpage.css" /> -->
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" charset="utf-8"></script>
+    <script src="jquery.onepage-scroll.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="onepage-scroll.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.2/vendors/jquery.easings.min.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.2/vendors/scrolloverflow.js" charset="utf-8"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.2/jquery.fullPage.min.js" charset="utf-8"></script> -->
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $(".main").onepage_scroll({
+          sectionContainer: "section",
+          easing: "ease-in-out",
+          animationTime: 1000,
+          pagination: true,
+          loop: true,
+          responsiveFallback: 600,
+          direction: "vertical"
+        });
+      });
+    </script>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg fixed-top">
+  <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-item nav-link active" href="#">Blog</a>
+      <a class="nav-item nav-link" href="#">About Us</a>
+    </div>
+  </div>
+</nav>
+    <div class="main">
+      <section class="page1">
+        <!-- <h1>This Page 1</h1> -->
+        <div class="overlay"></div>
+        <div class="content">
+          <div class="description">
+            <h1>HIMPUNAN MAHASISWA<br><span style="letter-spacing: 6.5px;">SISTEM INFORMASI</h1>
+              <p>Berfungsi sebagai wadah untuk<br>menyalurkan aspirasi media komunikasi<br>dan informasi serta pembelajaran organisasi<br>untuk pengembangan diri bagi anggotanya
+              </p>
+          </div>
+          <div class="division">
+            <div class="it">
+                <img src="img/IT.png" height="100px" alt="" class="logo-shadow"><br><br>
+                <a href="#">IT</a>
+            </div>
+            <div class="humas">
+              <img src="img/humas.png" height="100px" alt="" class="logo-shadow"><br><br>
+                <a href="#">Humas</a>
+            </div>
+            <div class="kms">
+              <img src="img/kms.png" height="100px" alt="" class="logo-shadow"><br><br>
+                <a href="#">Kemahasiswaan</a>
+            </div>
+            <div class="rtk">
+              <img src="img/rtk.png" height="100px" alt="" class="logo-shadow"><br><br>
+              <a href="#">Rumah Tangga</a>
+            </div>
+            <div class="kwu">
+              <img src="img/kwu.png" height="100px" alt="" class="logo-shadow"><br><br>
+              <a href="#">Kewirausahaan</a>
+            </div>
+          </div>
+
+        </div>
+      </section>
+      <section class="page2">
+        <div class="container">
+          <div class="desc-division">
+            <div class="desc-humas">
+              <img src="img/humas.png" height="100px" alt="" class="logo-shadow"><br>
+              Humas merupakan divisi yang menjalin hubungan dan kerjasama dengan mahasiswa dan masyarakat diluar ruang lingkup himpunan, memberikan informasi terkait dunia di luar himpunan serta memperkenalkan HMSI UNAND melalui kegiatan kegiatan akademik maupun non-akademik.
+            </div>
+            <div class="desc-it">
+              <img src="img/IT.png" height="100px" alt="" class="logo-shadow"><br>
+              Divisi Keprofesian dan IT merupakan divisi yang mewadahi mahasiswa dalam mengembangkan kemampuan dan bakat serta berperan penting bagi mahasiswa Sistem Informasi agar dapat terus mengenal atau mengetahui perkembangan teknologi.
+            </div>
+            <div class="desc-rtk">
+              <img src="img/rtk.png" height="100px" alt="" class="logo-shadow"><br>
+               RTK (Rumah Tangga dan Kesekretariatan) adalah divisi yang mengurusi semua hal mengenai kesekretariatan dan rumah tangga HMSI Unand seperti menata, memperindah serta memastikan ruang kesekretariatan HMSI bersih serta menjaga semua inventaris himpunan yang ada.
+            </div>
+            <div class="desc-kwu">
+              <img src="img/kwu.png" height="100px" alt="" class="logo-shadow"><br>
+              Kewirausahaan merupakan sebuah divisi di himpunan mahasiswa sistem informasi yang membantu menambah pemasukan dana himpunan. Hal ini secara tidak langsung dapat melatih pengurus dalam hal melihat peluang yang bisa dijadikan sebagai lapak usaha serta meningkatkan inovasi dan kreatifitas pengurus demi kemajuan HMSI kedepannya.
+            </div>
+            <div class="desc-kms">
+              <img src="img/kms.png" height="100px" alt="" class="logo-shadow"><br>
+              Kemahasiswaan merupakan divisi yang menghimpun anggota HMSI dalam mengoptimalkan dan memberdayakan seluruh potensi yang dimiliki melalui kegiatan yang berkaitan dengan non-akademik,meningkatkan peran aktif mahasiswa dalam berbagai kegiatan,serta dapat memberikan solusi terhadap masalah yang dihadapi anggota HMSI
+            </div>
+            <div class="logo-hmsi">
+              <img src="img/logo.png" height="100px" alt="" class="logo-shadow"><br>
+            </div>
+          </div>
+          </div>
+
+      </section>
+      <section class="page3">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="event">
+                <div class="title">
+                    <span class="title-event"><p class="title-event-ya">GRAMORIS</p><span class="nick-event">Graduation Ceremony of Information System</span> </span>
+
+
+                </div>
+                <div class="pict-event">
+                  <div class="circle-color" style="top:100px;">
+
+                  </div>
+                  <div class="circle" style="top:150px;right: 150px">
+                      <div class="text"></div>
+                  </div>
+                  <div class="circle two" style="top:400px;right: 450px">
+                      <div class="text">HELLO</div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item isce">
+              <div class="event">
+                <div class="title">
+                    <span class="title-event"><p class="title-event-ya">ISCE</p><span class="nick-event">Information System Celebration & Event</span> </span>
+                </div>
+                <div class="pict-event">
+                  <div class="circle-color" style="top:100px;">
+
+                  </div>
+                  <div class="circle" style="top:150px;right: 150px">
+                      <div class="text"></div>
+                  </div>
+                  <div class="circle two" style="top:400px;right: 450px">
+                      <div class="text">HELLO</div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="event">
+                <div class="title">
+                    <span class="title-event"><p class="title-event-ya">BAKSOS</p><span class="nick-event">Bakti Sosial</span> </span>
+                </div>
+                <div class="pict-event">
+                  <div class="circle-color" style="top:100px;">
+
+                  </div>
+                  <div class="circle" style="top:150px;right: 150px">
+                      <div class="text"></div>
+                  </div>
+                  <div class="circle two" style="top:400px;right: 450px">
+                      <div class="text">HELLO</div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </section>
+      <section class="page4">
+        <div class="overlay overlay-2">
+          <div class="content-blog" style="position:absolute;bottom:50px;left:0; right:0">
+            <h1 class="title-blog">Misi</h1>
+            <p style="font-size:1.2em;">Membantu pelaksanaan Tri Dharma Perguruan Tinggi sesuai dengan bidangnya mahasiswa Sistem Informasi;<br>
+              Meningkatkan kualitas SDM baik secara intelektualitas maupun profesionalisme;</p>
+          </div>
+        </div>
+        <div class="overlays overlay-2">
+          <div class="content-blog" style="position:absolute;top:50px;left:0; right:0;color:grey">
+            <h1 class="title-blog">Visi</h1>
+            <p style="font-size:1.2em;">Meningkatkan mutu dan kualitas anggota sesuai dengan bidangnya;<br>
+              Meningkatkan rasa persaudaraan diantara anggotanya;<br>
+              Membantu pelaksanaan Tri Dharma Perguruan Tinggi sesuai dengan bidangnya;</p>
+          </div>
+        </div>
+      </section>
+      <section class="page5">
+        <div class="content-contact">
+          <div class="title-contact">
+            <h3 class="title-event-ya" style="font-size:1.5em;letter-spacing: 2px;">HIMPUNAN MAHASISWA SISTEM INFORMASI<br>UNIVERSITAS ANDALAS</h3>
+          </div>
+          <div class="add-hmsi">
+            <br>
+            <table>
+              <tr>
+                <td><b>Alamat</b></td>
+                <td>:</td>
+                <td>Gedung E lantai 2- Fakultas Teknologi Informasi<br>Jalan Universitas Andalas, Limau Manis, Pauh, Kota Padang, Sumatera Barat 25163</td>
+              </tr>
+              <tr>
+                <td><b>E-mail</b></td>
+                <td>:</td>
+                <td>official.siunand@gmail.com</td>
+              </tr>
+              <tr>
+                <td><b>Hours</b></td>
+                <td>:</td>
+                <td>9:00 am – 5:00 pm</td>
+              </tr>
+            </table>
+            <br>
+            <br>
+            <div class="icon-sosmed">
+              <div class="instagram">
+                <img src="img/ig-icon.png" alt="" width="50">
+              </div>
+              <div class="twitter">
+                <img src="img/web-icon.png" alt="" width="50">
+              </div>
+              <div class="facebook">
+                <img src="img/fb-icon.png" alt="" width="50">
+              </div>
+              <div class="line">
+              <img src="img/line-icon.png" alt="" width="50">
+              </div>
+            </div>
+          </div>
+          <div class="send-hmsi">
+            <br>
+            <h3>Send Us A Message</h3>
+            <div class="send-message">
+              <input type="text" name="" value="" class="form-control" placeholder="Nama Lengkap"><br>
+              <input type="text" name="" value="" class="form-control" placeholder="E-mail"><br>
+              <input type="text" name="" value="" class="form-control" placeholder="Pesan"><br>
+              <button type="button" name="button" class="form-control send-button">Send Message</button>
+            </div>
+          </div>
+        </div>
+        <div class="overlay-3">
+          <div class="content-blog blog" style="bottom:50px;left:0; right:0;">
+            <div class="title-blogs">
+              <h1 style="text-align:left;margin:0">See what's happening</h1>
+            </div>
+            <div class="content-blogs" style="text-align:right;">
+              <a href="#" class="button-blog">Kunjungi Blog</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
